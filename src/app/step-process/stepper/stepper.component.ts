59a2874efa9d59e978d7183efb0968cb82e157e7
Mrcha <31120019+mrcha1805@users.noreply.github.com>
2022-07-21 15:59:43 +0700

    stepper ui

--- src/app/step-process/stepper/stepper.component.ts
+++ src/app/step-process/stepper/stepper.component.ts
@@ -8,8 +8,25 @@ import { Component, OnInit } from '@angular/core';
 export class StepperComponent implements OnInit {
 
   constructor() { }
-
+  stepCurrent!: number;
   ngOnInit(): void {
+    this.stepCurrent = 1;
+  }
+
+  stepDisplay(tab:number, current: number) {
+
+    if ((tab === 1) || ((tab === 4) && (current === 4))) {
+      return '../../../assets/images/step-active.svg';
+    }
+    if (tab <= current) {
+      return '../../../assets/images/step-continue.svg';
+    } else {
+      return '../../../assets/images/step.svg';
+    }
+  }
+
+  stepAction(index: number) {
+    this.stepCurrent = index;
   }
 
 }
