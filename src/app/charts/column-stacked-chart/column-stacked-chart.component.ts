67a25987a121f221e1e9470917105a3d0b3255fa
Mrcha <31120019+mrcha1805@users.noreply.github.com>
2022-07-25 15:50:28 +0700

    column stacked chart

--- src/app/charts/column-stacked-chart/column-stacked-chart.component.ts
+++ src/app/charts/column-stacked-chart/column-stacked-chart.component.ts
@@ -33,26 +33,45 @@ export class ColumnStackedChartComponent implements OnInit {
     this.chartOptions = {
       series: [
         {
-          name: "PRODUCT A",
-          data: [44, 55, 41, 67, 22, 43]
+          name: "Travel",
+          data: [44, 55, 41, 67, 22, 43, 12, 10, 15, 39, 20, 35, 67]
         },
         {
-          name: "PRODUCT B",
-          data: [13, 23, 20, 8, 13, 27]
+          name: "Life style dinning",
+          data: [13, 23, 20, 8, 13, 27, 50, 43, 61, 98, 12, 41, 60],
+     
         },
         {
-          name: "PRODUCT C",
-          data: [11, 17, 15, 15, 21, 14]
+          name: "Investment",
+          data: [11, 17, 15, 15, 21, 14, 12, 5, 9, 11, 45, 80, 78]
         },
         {
-          name: "PRODUCT D",
-          data: [21, 7, 25, 13, 22, 8]
+          name: "Music",
+          data: [21, 7, 25, 13, 22, 8, 17, 15, 15, 21, 14, 12, 5]
+        },
+        {
+          name: "Game",
+          data: [44, 55, 41, 67, 22, 43, 20, 8, 13, 27, 50, 43, 61]
+        },
+        {
+          name: "Working",
+          data: [13, 23, 20, 8, 13, 27, 11, 17, 15, 13, 27, 50, 39]
+        },
+        {
+          name: "Streaming",
+          data: [11, 17, 15, 15, 21, 14, 20, 8, 13, 27, 11, 17, 15]
+        },
+        {
+          name: "Shopping online",
+          data: [21, 7, 25, 13, 22, 8, 23, 20, 8, 13, 27, 50, 43]
         }
       ],
       chart: {
         type: "bar",
-        height: 350,
+        height: 450,
+        width: 800,
         stacked: true,
+        stackType: "100%",
         toolbar: {
           show: true
         },
@@ -62,7 +81,7 @@ export class ColumnStackedChartComponent implements OnInit {
       },
       responsive: [
         {
-          breakpoint: 480,
+          breakpoint: 1200,
           options: {
             legend: {
               position: "bottom",
@@ -74,18 +93,26 @@ export class ColumnStackedChartComponent implements OnInit {
       ],
       plotOptions: {
         bar: {
-          horizontal: false
+          horizontal: false,
+ 
         }
       },
       xaxis: {
         type: "category",
         categories: [
-          "01/2011",
-          "02/2011",
-          "03/2011",
-          "04/2011",
-          "05/2011",
-          "06/2011"
+          "< 17",
+          "17-22",
+          "23-25",
+          "26-30",
+          "31-35",
+          "36-40",
+          "41-45",
+          "46-50",
+          "51-55",
+          "56-60",
+          "61-65",
+          "66-70",
+          ">70"
         ]
       },
       legend: {
