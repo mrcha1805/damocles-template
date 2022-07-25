b1dba411ed2d73cccf130a607ea2f2abb3ab1d41
Mrcha <31120019+mrcha1805@users.noreply.github.com>
2022-07-25 10:13:05 +0700

    card info kpi

--- src/app/modules/dashboard/dashboard.component.ts
+++ src/app/modules/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgxPopperjsPlacements, NgxPopperjsTriggers } from 'ngx-popperjs';
 
 @Component({
   selector: 'app-dashboard',
@@ -8,8 +9,41 @@ import { Component, OnInit } from '@angular/core';
 export class DashboardComponent implements OnInit {
 
   constructor() { }
-
+  triggers = NgxPopperjsTriggers;
+  placements = NgxPopperjsPlacements;
+  offsetModifiers = [
+    {
+      name: 'offset',
+      options: {
+        offset: [0, 2],
+      },
+    },
+    {
+      name: 'flip',
+      options: {
+        flipVariations: false,
+      },
+    },
+  ];
+  kpiSelected= "";
+  dropdownIconSrc ="../../../assets/icons/dropdown.svg";
+  project = [
+    {
+      name: 'Test KPI',
+      value: 0
+    },
+    {
+      name: 'ChartB',
+      value: 1
+    },
+    {
+      name: 'ChartC',
+      value: 2
+    },
+  ]
+  dropdownShow = false;
   ngOnInit(): void {
+    this.kpiSelected = this.project[0].name;
   }
 
 }
