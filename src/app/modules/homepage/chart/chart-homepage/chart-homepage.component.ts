0a20d4687ffcbe861bb45c89fa17f2a3f8020e93
PANJAWAT-SEWIKUL <asvan0747@gmail.com>
2022-08-17 10:43:07 +0700

    fix event resize lib on responsive page

--- src/app/modules/homepage/chart/chart-homepage/chart-homepage.component.ts
+++ src/app/modules/homepage/chart/chart-homepage/chart-homepage.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  HostListener,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { ResizeEvent } from 'angular-resizable-element';
 
 @Component({
@@ -7,31 +13,46 @@ import { ResizeEvent } from 'angular-resizable-element';
   styleUrls: ['./chart-homepage.component.scss'],
 })
 export class ChartHomepageComponent implements OnInit {
+  @ViewChild('chart', { static: true }) element: ElementRef | any;
   style: object = {};
+  countDefault: number = 0;
+  MIN_DIMENSIONS_PX: number = 0;
 
   constructor() {}
 
   ngOnInit(): void {}
 
-  validate(event: ResizeEvent): boolean {
-    const MIN_DIMENSIONS_PX: number = 0;
+  validate = (event: ResizeEvent): boolean => {
+    this.countDefault += 1;
+    this.MIN_DIMENSIONS_PX =
+      this.countDefault === 1
+        ? this.element.nativeElement.offsetHeight
+        : this.MIN_DIMENSIONS_PX;
     if (
-      event.rectangle.width &&
       event.rectangle.height &&
-      (event.rectangle.width < MIN_DIMENSIONS_PX ||
-        event.rectangle.height < MIN_DIMENSIONS_PX)
+      event.rectangle.height < this.MIN_DIMENSIONS_PX
     ) {
       return false;
     }
     return true;
-  }
+  };
 
-  onResizeEnd(event: ResizeEvent): void {
+  onResizeEnd = (event: ResizeEvent): void => {
     this.style = {
       left: `${event.rectangle.left}px`,
       top: `${event.rectangle.top}px`,
       width: `${event.rectangle.width}px`,
       height: `${event.rectangle.height}px`,
+    };
+  };
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    this.countDefault = 0;
+    this.onResizeEnd = (event: ResizeEvent): void => {
+      this.style = {
+        height: '100%',
+      };
     };
   }
 }
